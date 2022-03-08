cd5a095916559c2af58073ebb3b704e46a141e63
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2022-03-08 14:53:52 +0630

    run sidekiq work

--- app/workers/hard_job.rb
+++ app/workers/hard_job.rb
@@ -8,7 +8,31 @@ class HardJob
   include Sidekiq::Job
 
   def perform
-    puts 'arrrrrrrrrrrrrrrrrrrWWWW'
+
+    allfeedbacks = Feedback.includes(:user)
+  
+    xml_content = Nokogiri::XML::Builder.new { |xml| 
+      xml.body do
+          allfeedbacks.each do |feedback|
+              xml.feedback do
+                xml.feedbackID feedback.id
+                xml.ownername User.where(:id => feedback.owner_id).pluck(:username).join
+                xml.ownercomment feedback.owner_comment
+                if feedback.post_id.nil?
+                xml.type "user"
+                else
+                xml.type "post"
+                end
+              end
+          end
+      end
+    }.to_xml
+
+    puts xml_content
+    File.open('feedback.xml', 'wb') {|f| f.write xml_content }
+
+
+
   end
 
 end
